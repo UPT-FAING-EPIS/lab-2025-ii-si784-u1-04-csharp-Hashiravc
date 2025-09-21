371b83784330495e51e4702333ffc8ca05dd3ccc
Hashiravc <hv2022075480@virtual.upt.pe>
2025-09-21 18:57:08 -0500

    Agregar workflow de CI/CD y archivos de despliegue

---
+++ src/Areas/Domain/ShortenContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Shorten.Areas.Domain;
+
+/// <summary>
+/// Clase de infraestructura que representa el contexto de la base de datos
+/// </summary>
+public class ShortenContext : DbContext
+{
+    /// <summary>
+    /// Constructor de la clase
+    /// </summary>
+    /// <param name="options">Opciones de conexión de BD</param>
+    public ShortenContext(DbContextOptions<ShortenContext> options) : base(options)
+    {
+    }
+
+    /// <summary>
+    /// Propiedad que representa la tabla de mapeo de urls
+    /// </summary>
+    public DbSet<UrlMapping> UrlMappings { get; set; }
+}
